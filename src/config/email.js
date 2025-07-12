4ff6c381072b224a11b81811699f730819d88dc8
grace363 <gracewambuigw170@gmail.com>
2025-07-12 21:23:33 +0300

    Create email.js

---
+++ src/config/email.js
@@ -0,0 +1,15 @@
+```javascript
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransporter({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+module.exports = transporter;
+```
