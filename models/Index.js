57e544dce37a866d9acf47343c77f428bc0cb185
grace363 <gracewambuigw170@gmail.com>
2025-07-12 21:29:33 +0300

    Update Index.js

--- models/Index.js
+++ models/Index.js
@@ -1,1 +1,11 @@
-
+```javascript
+module.exports = {
+  User: require('./User'),
+  Video: require('./Video'),
+  WatchHistory: require('./WatchHistory'),
+  PaymentRequest: require('./PaymentRequest'),
+  AppSettings: require('./AppSettings'),
+  Admin: require('./Admin'),
+  OwnerEarnings: require('./OwnerEarnings')
+};
+```
